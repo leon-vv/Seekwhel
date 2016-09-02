d2e03f459a226d194317fb0534b4b18cf3f2a361
Léon van Velzen <leon-vv@hotmail.com>
2016-09-02 22:22:05 +0200

    Install pretty printers for queries

--- utop-seekwhel.ml
+++ utop-seekwhel.ml
@@ -13,3 +13,17 @@ end
 
 module S = Seekwhel.Make(Seekwhel_test)
 open S
+
+let pp_of_to_string f =
+    (fun formatter input -> Format.pp_print_text formatter (f input)) ;;
+
+
+let pp_select = pp_of_to_string (S.Select.to_string) ;;
+let pp_update = pp_of_to_string (S.Update.to_string) ;;
+let pp_insert = pp_of_to_string (S.Insert.to_string) ;;
+let pp_delete = pp_of_to_string (S.Delete.to_string) ;;
+
+#install_printer pp_select ;;
+#install_printer pp_update ;;
+#install_printer pp_insert ;;
+#install_printer pp_delete ;;

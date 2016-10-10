3c75b334fd6a20086731c2d4e79325ef315ffab8
Léon van Velzen <leon-vv@hotmail.com>
2016-10-10 22:07:44 +0200

    Make utop-seekwhel.ml not throw errors

--- utop-seekwhel.ml
+++ utop-seekwhel.ml
@@ -1,30 +1,42 @@
-#use "topfind";;
 #require "postgresql";;
 #require "calendar";;
-#mod_use "src/keywords.ml";;
-#mod_use "src/seekwhel.ml";;
-#mod_use "tests.ml";;
 
-let soe = Tests.S.Select.string_of_expr;;
+#mod_use "src/seekwhelInner.ml";;
+#mod_use "src/seekwhelConnection.ml";;
+#mod_use "src/seekwhelInner.ml";;
+#mod_use "src/seekwhelKeywords.ml";;
+#mod_use "src/seekwhelColumn.ml";;
+
+#mod_use "src/seekwhelSelect.ml";;
+#mod_use "src/seekwhelUpdate.ml";;
+#mod_use "src/seekwhelDelete.ml";;
+#mod_use "src/seekwhelInsert.ml";;
+
+#mod_use "src/seekwhel.ml";;
+#mod_use "maintenance/queries.ml"
+#mod_use "maintenance/tests.ml"
+
+open CalendarLib
+
+open Queries
+open S
+open S.Select
+
+module SI = SeekwhelInner
+module SC = SeekwhelColumn
+
 module TS = Tests.String_of_expr_test;;
 
-open TS
-
 module Seekwhel_test : Seekwhel.Connection = struct
-    let connection = new Postgresql.connection
+    let conn = new Postgresql.connection
 	~host:"127.0.0.1"
 	~dbname:"seekwhel_test"
 	~user:"seekwhel_test"
 	~password:"seekwhel_test" ()
 end
 
-module S = Seekwhel.Make(Seekwhel_test)
-open S
-open S.Select
-
 let pp_of_to_string f =
     (fun formatter input -> Format.pp_print_text formatter (f input)) ;;
-
 
 let pp_select = pp_of_to_string (S.Select.to_string) ;;
 let pp_update = pp_of_to_string (S.Update.to_string) ;;
